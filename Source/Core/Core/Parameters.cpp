724be620a1b02c69d0187015f0433160000e8796
Xe24vc <Xe24vc@users.noreply.github.com>
2020-09-24 20:26:24 -0700

    More Settings
    Additional Ini config.

--- Source/Core/Core/Parameters.cpp
+++ Source/Core/Core/Parameters.cpp
@@ -1,11 +1,17 @@
 #include "Parameters.h"
 #include "Core/BootManager.h"
 #include "Core/ConfigManager.h"
+#include "Core/Config/GraphicsSettings.h"
 #include "Common/IniFile.h"
 #include <iostream>
 #include <fstream>
 
+bool EFBAccessenable;
+bool EFBEmulateFormatChanges;
+bool EFBToTextureenable;
+int DeferEFBCopies;
 int ImmediateXFBenable;
+bool DeferEFB = false;
 bool ImmediateXFB = false;
 
 using std::ifstream;
@@ -31,11 +37,51 @@ void Parameters::Set::GameIni(std::vector<std::string> game)
   
   core_section->Get("Overclock", &StartUp.m_OCFactor, StartUp.m_OCFactor);
   core_section->Get("OverclockEnable", &StartUp.m_OCEnable, StartUp.m_OCEnable);
+  video_hacks_section->Get("EFBAccessenable", &EFBAccessenable, EFBAccessenable);
+  video_hacks_section->Get("EFBEmulateFormatChanges", &EFBEmulateFormatChanges, EFBEmulateFormatChanges);
+  video_hacks_section->Get("EFBToTextureenable", &EFBToTextureenable, EFBToTextureenable);
+  video_hacks_section->Get("DeferEFBCopies", &DeferEFB, DeferEFB);
   video_hacks_section->Get("ImmediateXFBenable", &ImmediateXFB, ImmediateXFB);
+  
+  if (EFBAccessenable == true)
+  {
+    Config::SetCurrent(Config::GFX_HACK_EFB_ACCESS_ENABLE,
+      !Config::Get(Config::GFX_HACK_EFB_ACCESS_ENABLE));
+  }
+  
+  if (EFBEmulateFormatChanges == true)
+  {
+    Config::SetCurrent(Config::GFX_HACK_EFB_EMULATE_FORMAT_CHANGES,
+      !Config::Get(Config::GFX_HACK_EFB_EMULATE_FORMAT_CHANGES));
+  }
+  
+  if (EFBToTextureenable == true)
+  {
+    Config::SetCurrent(Config::GFX_HACK_SKIP_EFB_COPY_TO_RAM,
+      !Config::Get(Config::GFX_HACK_SKIP_EFB_COPY_TO_RAM));
+  }
+  
+  if (DeferEFB == true)
+  {
+    DeferEFBCopies = 1;
+  }
   
   if (ImmediateXFB == true)
   {
     ImmediateXFBenable = 1;
+  }
+}
+
+bool Parameters::Get::DeferEFB()
+{
+  if (DeferEFBCopies == 1)
+  {
+    return true;
+  }
+  
+  else
+  {
+    return false;
   }
 }
 
